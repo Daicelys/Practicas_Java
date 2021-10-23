3aea55ccbb6ec0ed33bc89f815b4cb4dc2365f2f
Daicelys <daicelys@martinez.com>
2021-10-23 02:58:48 -0400

    Segundo arreglo

--- main/Main.java
+++ main/Main.java
@@ -8,8 +8,15 @@ import lista.ListaPersona;
 public class Main {
 	//MODIFICATE
 	public static void main(String[] args) {
-	
-		ListaPersona lista = new ListaPersona();
+		int cantPersona;
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite la cantidad de personas");
+		cantPersona= entrada.nextInt();
+for (int i=0 ; i<cantPersona; i++) {
+	ListaPersona lista = new ListaPersona();	
+		}
+		
+		
 	}		
 
 }	
